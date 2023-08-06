523f37473d35394d60550994f65c563401a8d550
Yugps <yugpratapsingh8@gmail.com>
2023-08-07 01:12:57 +0530

    deleted unnecessary things

--- app.py
+++ app.py
@@ -1,77 +1,39 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs 
+import os 
+from flask import Flask,request,render_template
 import requests
-from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
 import logging
-import pymongo
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
-import os
 
-app = Flask(__name__)
+logging.basicConfig(filename='image_scrapper_logger.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s %(message)s')
+app=Flask(__name__)
 
-@app.route("/", methods = ['GET'])
+@app.route('/',methods=['GET'])
 def homepage():
-    return render_template("index.html")
+    return render_template('index.html')
+logging.info(msg='Home page rendered')
+   
+@app.route('/review',methods=['POST'])
+def main_function():
+    if request.method=='POST':
+        query=request.form['content']
+        final_query=query.replace('',' ')
+        logging.info('search querry recieved')
+        image_url=f'https://www.google.com/search?q={final_query}&tbm=isch&ved=2ahUKEwj5oOaokMaAAxW66DgGHQyVAkgQ2-cCegQIABAA&oq=elonmusk&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEIoFEEM6BAgjECc6BggAEAcQHjoICAAQBRAHEB46BwgAEBgQgAQ6CQgAEBgQgAQQClDgmWBY5rlgYK69YGgCcAB4AIABzAGIAfAKkgEFMC45LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=HpLOZPmZHbrR4-EPjKqKwAQ&bih=754&biw=1482'
+        image_page_html=bs(requests.get(image_url).text,'html.parser')
+        images=image_page_html.find_all('img')
+        del images[0]
+        image_sources=[]
+        for image in images:
+            image_sources.append(image['src'])
+        os.makedirs(f'{query}_images')
+        logging.info('Directory for saving the images have been created')
+        for n,i in enumerate(image_sources): # This will store all the images in local directory
+            with open(f'{query}_images/image_{n}.jpg','wb') as f:
+                f.write(urlopen(i).read())
+                f.close()
+        logging.info('Images have been saved to the local directory')
+    return 'All Images have been saved in your local directory'
 
-@app.route("/review" , methods = ['POST' , 'GET'])
-def index():
-    if request.method == 'POST':
-                try:
-
-                    # query to search for images
-                    query = request.form['content'].replace(" ","")
-
-                            # directory to store downloaded images
-                    save_directory = "images/"
-
-                            # create the directory if it doesn't exist
-                    if not os.path.exists(save_directory):
-                        os.makedirs(save_directory)
-
-
-
-                            # fake user agent to avoid getting blocked by Google
-                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-
-                            # fetch the search results page
-                    response = requests.get(f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M")
-
-
-                            # parse the HTML using BeautifulSoup
-                    soup = BeautifulSoup(response.content, "html.parser")
-
-                            # find all img tags
-                    image_tags = soup.find_all("img")
-
-                            # download each image and save it to the specified directory
-                    del image_tags[0]
-                    img_data=[]
-                    for index,image_tag in enumerate(image_tags):
-                                # get the image source URL
-                                image_url = image_tag['src']
-                                #print(image_url)
-                                
-                                # send a request to the image URL and save the image
-                                image_data = requests.get(image_url).content
-                                mydict={"Index":index,"Image":image_data}
-                                img_data.append(mydict)
-                                with open(os.path.join(save_directory, f"{query}_{image_tags.index(image_tag)}.jpg"), "wb") as f:
-                                    f.write(image_data)
-                    client = pymongo.MongoClient("mongodb+srv://snshrivas:Snshrivas@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-                    db = client['image_scrap']
-                    review_col = db['image_scrap_data']
-                    review_col.insert_many(img_data)          
-
-                    return "image laoded"
-                except Exception as e:
-                    logging.info(e)
-                    return 'something is wrong'
-            # return render_template('results.html')
-
-    else:
-        return render_template('index.html')
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8000)
